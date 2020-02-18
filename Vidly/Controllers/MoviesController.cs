e94705f6032a5a4ec89520dd6b12f7d99043e141
Groof,Marcel M. de <m.degroof@student.fontys.nl>
2020-02-18 20:40:44 +0100

    Add project files.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!"};
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+
+            };
+
+            return View(viewModel);
+        }
+
+        public ViewResult Index()
+        {
+
+            var movies = GetMovies();
+
+            return View(movies);
+
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Shrek!"},
+                new Movie {Id = 2, Name = "Pulp Fiction"},
+                new Movie {Id = 3, Name = "JoJo Rabbit"}
+            };
+        }
+
+        //public ActionResult ByReleaseDate(int year, int month)
+        //{
+        //    return Content(year +"/"+month);
+        //}
+    }
+}
